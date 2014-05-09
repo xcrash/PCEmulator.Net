f48e395ff6bdb781fcde3110793ca6138ba93e16
xcrash <grishinage@gmail.com>
2014-05-09 15:31:59 +0400

    Serial port implemented

--- src/PCEmulator.Net/Serial.cs
+++ src/PCEmulator.Net/Serial.cs
@@ -1,52 +1,195 @@
 ﻿using System;
+using System.Text;
 
 namespace PCEmulator.Net
 {
+	/// <summary>
+	/// Serial Controller Emulator
+	/// </summary>
 	public class Serial
 	{
 		private int divider;
-		private int rbr;
-		private int ier;
-		private int iir;
-		private int lcr;
-		private int lsr;
-		private int msr;
-		private int scr;
-		private object set_irq_func;
-		private object write_func;
-		private string receive_fifo;
+		private byte rbr;
+		private byte ier;
+		private byte iir;
+		private byte lcr;
+		private byte lsr;
+		private byte msr;
+		private byte scr;
+		private readonly Action<bool> setIrqFunc;
+		private readonly Action<char> writeFunc;
+		private string receiveFifo;
+		private byte mcr;
 
-		public Serial(PCEmulator Ng, int i, Action<bool> kh, object lh)
+		public Serial(PCEmulator ng, int port, Action<bool> kh, Action<char> lh)
 		{
-			this.divider = 0;
-			this.rbr = 0;
-			this.ier = 0;
-			this.iir = 0x01;
-			this.lcr = 0;
+			divider = 0;
+			rbr = 0;
+			ier = 0;
+			iir = 0x01;
+			lcr = 0;
 			//this.mcr;
-			this.lsr = 0x40 | 0x20;
-			this.msr = 0;
-			this.scr = 0;
-			this.set_irq_func = kh;
-			this.write_func = lh;
-			this.receive_fifo = "";
-			Ng.register_ioport_write(0x3f8, 8, 1, ioport_write);
-			Ng.register_ioport_read(0x3f8, 8, 1, ioport_read);
+			lsr = 0x40 | 0x20;
+			msr = 0;
+			scr = 0;
+			setIrqFunc = kh;
+			writeFunc = lh;
+			receiveFifo = "";
+			ng.register_ioport_write(port, 8, 1, ioport_write);
+			ng.register_ioport_read(port, 8, 1, ioport_read);
 		}
 
-		public void send_chars(char str)
+		private void update_irq()
 		{
-			throw new NotImplementedException();
+			if ((lsr & 0x01) != 0 && (ier & 0x01) != 0)
+			{
+				iir = 0x04;
+			}
+			else if ((lsr & 0x20) != 0 && (ier & 0x02) != 0)
+			{
+				iir = 0x02;
+			}
+			else
+			{
+				iir = 0x01;
+			}
+// ReSharper disable once ConvertIfStatementToConditionalTernaryExpression
+			if (iir != 0x01)
+			{
+				setIrqFunc(true);
+			}
+			else
+			{
+				setIrqFunc(false);
+			}
+		}
+
+		private void ioport_write(uint mem8Loc, byte x)
+		{
+			mem8Loc &= 7;
+			switch (mem8Loc)
+			{
+				default:
+// ReSharper disable once RedundantCaseLabel
+				case 0:
+					if ((lcr & 0x80) != 0)
+					{
+						divider = (divider & 0xff00) | x;
+					}
+					else
+					{
+						lsr &= Convert.ToByte(~0x20);
+						update_irq();
+						writeFunc(Encoding.ASCII.GetString(new[] {x})[0]);
+						lsr |= 0x20;
+						lsr |= 0x40;
+						update_irq();
+					}
+					break;
+				case 1:
+					if ((lcr & 0x80) != 0)
+					{
+						divider = (divider & 0x00ff) | (x << 8);
+					}
+					else
+					{
+						ier = x;
+						update_irq();
+					}
+					break;
+				case 2:
+					break;
+				case 3:
+					lcr = x;
+					break;
+				case 4:
+					mcr = x;
+					break;
+				case 5:
+					break;
+				case 6:
+					msr = x;
+					break;
+				case 7:
+					scr = x;
+					break;
+			}
 		}
 
 		private byte ioport_read(uint mem8Loc)
 		{
-			throw new NotImplementedException();
+			byte res;
+			mem8Loc &= 7;
+			switch (mem8Loc)
+			{
+				default:
+// ReSharper disable once RedundantCaseLabel
+				case 0:
+					if ((lcr & 0x80) != 0)
+					{
+						res = (byte) (divider & 0xff);
+					}
+					else
+					{
+						res = rbr;
+						lsr &= Convert.ToByte(~(0x01 | 0x10));
+						update_irq();
+						send_char_from_fifo();
+					}
+					break;
+				case 1:
+					if ((lcr & 0x80) != 0)
+					{
+						res = (byte) ((divider >> 8) & 0xff);
+					}
+					else
+					{
+						res = ier;
+					}
+					break;
+				case 2:
+					res = iir;
+					break;
+				case 3:
+					res = lcr;
+					break;
+				case 4:
+					res = mcr;
+					break;
+				case 5:
+					res = lsr;
+					break;
+				case 6:
+					res = msr;
+					break;
+				case 7:
+					res = scr;
+					break;
+			}
+			return res;
 		}
 
-		private void ioport_write(uint mem8Loc, byte data)
+		private void send_char(char mh)
 		{
-			throw new NotImplementedException();
+			rbr = Convert.ToByte(mh);
+			lsr |= 0x01;
+			update_irq();
+		}
+
+		private void send_char_from_fifo()
+		{
+			var nh = receiveFifo;
+			if (nh != "" && (lsr & 0x01) == 0)
+			{
+				send_char(nh[0]);
+				receiveFifo = nh.Substring(1, nh.Length - 1);
+			}
+		}
+
+		public void send_chars(char na)
+		{
+			receiveFifo += na;
+			send_char_from_fifo();
 		}
 	}
 }
