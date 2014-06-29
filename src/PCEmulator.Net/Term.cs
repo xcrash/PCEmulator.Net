38e9907887a50b8b5c30099ee3f6263b2b60c6b0
xcrash <grishinage@gmail.com>
2014-06-29 11:59:20 +0400

    remove unused/commented code

--- src/PCEmulator.Net/Term.cs
+++ src/PCEmulator.Net/Term.cs
@@ -18,20 +18,15 @@ namespace PCEmulator.Net
 		private int column;
 
 		private char[] buffer;
-//		private char[][] lines;
 		private int[][] lines;
 		private int scrollTop, scrollBottom;
-		private List<int> tabStops;
-		private int saveRow, saveColumn;
 
 		private string escapeChars, escapeArgs;
 
 		private bool started;
-		private bool vt52Mode;
-		private bool autoWrapMode;
 		private readonly int defaultWidth;
 
-		private int state = 0;
+		private int state;
 
 		private int lastCol = -1;
 		private int lastRow = -1;
@@ -43,11 +38,9 @@ namespace PCEmulator.Net
 		private int cur_param;
 		private string output_queue;
 		private int def_attr = (7 << 3) | 0;
-		private string[] fg_colors = new [] {"#000000", "#ff0000", "#00ff00", "#ffff00", "#0000ff", "#ff00ff", "#00ffff", "#ffffff"};
-		private string[] bg_colors = new [] {"#000000", "#ff0000", "#00ff00", "#ffff00", "#0000ff", "#ff00ff", "#00ffff", "#ffffff"};
 
-		private ConsoleColor[] fgColors = new[] { ConsoleColor.Black, ConsoleColor.Red, ConsoleColor.Green, ConsoleColor.Yellow, ConsoleColor.Blue, ConsoleColor.Magenta, ConsoleColor.Cyan, ConsoleColor.White };
-		private ConsoleColor[] bgColors = new[] { ConsoleColor.Black, ConsoleColor.Red, ConsoleColor.Green, ConsoleColor.Yellow, ConsoleColor.Blue, ConsoleColor.Magenta, ConsoleColor.Cyan, ConsoleColor.White };
+		private ConsoleColor[] fgColors = { ConsoleColor.Black, ConsoleColor.Red, ConsoleColor.Green, ConsoleColor.Yellow, ConsoleColor.Blue, ConsoleColor.Magenta, ConsoleColor.Cyan, ConsoleColor.White };
+		private ConsoleColor[] bgColors = { ConsoleColor.Black, ConsoleColor.Red, ConsoleColor.Green, ConsoleColor.Yellow, ConsoleColor.Blue, ConsoleColor.Magenta, ConsoleColor.Cyan, ConsoleColor.White };
 		private int cur_h;
 		private int tot_h = 1000;
 
@@ -62,77 +55,6 @@ namespace PCEmulator.Net
 			Reset();
 			BeginReadFromConsole();
 		}
-
-//		public void Write(char ch)
-//		{
-//			if (escapeChars != null)
-//			{
-//				ProcessEscapeCharacter(ch);
-//				return;
-//			}
-//
-//			const int za = 0;
-//			const int Aa = 1;
-//			const int Ba = 2;
-//
-//			switch (state)
-//			{
-//				case za:
-//					switch ((int)ch)
-//					{
-//						case 27:
-//							state = Aa;
-//							break;
-//
-//					}
-//					break;
-//				case Aa:
-//					state = ch == 91 ? Ba : za;
-//					break;
-//				case Ba:
-//					if (ch >= 48 && ch <= 57)
-//					{
-//					}
-//					else
-//					{
-//						if (ch == 59)
-//							break;
-//						state = za;
-//					}
-//					break;
-//			}
-//
-//			switch (ch)
-//			{
-//				case '\x1b':
-//					escapeChars = "";
-//					escapeArgs = "";
-//					break;
-//				case '\r':
-//					column = 0;
-//					break;
-//				case '\n':
-//					NextRowWithScroll();
-//					break;
-//
-//				case '\t':
-//					column = (from stop in tabStops where stop > column select (int?) stop).Min() ?? width - 1;
-//					break;
-//
-//				default:
-//					BufferAt(row, column, ch);
-//
-//					if (++column >= width)
-//						if (autoWrapMode)
-//						{
-//							column = 0;
-//							NextRowWithScroll();
-//						}
-//						else
-//							column--;
-//					break;
-//			}
-//		}
 
 		public void Write(char @char)
 		{
@@ -416,10 +338,6 @@ namespace PCEmulator.Net
 			column = 0;
 			scrollTop = 0;
 			scrollBottom = height;
-			vt52Mode = false;
-			autoWrapMode = true;
-			tabStops = new List<int>();
-			saveRow = saveColumn = 0;
 			ResetBufferAndWindows();
 		}
 
@@ -437,376 +355,137 @@ namespace PCEmulator.Net
 			Console.SetBufferSize(width, height);
 		}
 
-		private void BufferAt(int row, int column, char x)
-		{
-			if (row < 0 || row >= height || column < 0 || column >= width)
-				return;
-
-			if (column != width - 1 && row != height - 1)
-			{
-				if (lastCol + 1 != column || lastRow != row)
-					Console.SetCursorPosition(column, row);
-				Console.Write(x);
-				lastCol = column;
-				lastRow = row;
-			}
-			else
-			{
-				//TODO: what to do with bottom right char?
-				//see: http://stackoverflow.com/questions/739526/disabling-scroll-with-system-console-write
-			}
-			buffer[row * width + column] = x;
-		}
-
 		private void Refresh(int ka, int la)
 		{
 			int y;
 			for (y = ka; y <= la; y++)
 			{
-				var ta = y + y_disp;
-				if (ta >= cur_h)
-					ta -= cur_h;
-				var ia = lines[ta];
+				RefreshLine(y);
+			}
+		}
+
+		private void RefreshLine(int y)
+		{
+			var ta = y + y_disp;
+			if (ta >= cur_h)
+				ta -= cur_h;
+			var ia = lines[ta];
 //				var na = "";
-				var naa = new List<ConsoleItem>();
-				var w = width;
-				var oa = -1;
-				var qa = def_attr;
-				int i;
-				ConsoleItem taa = new ConsoleItem();
-				for (i = 0; i < w; i++)
-				{
-					var c = ia[i];
-					var pa = (int) c >> 16;
-					c &= 0xffff;
-					if (i == oa)
-					{
-						pa = -1;
-					}
-					if (pa != qa)
-					{
-						if (qa != def_attr)
-						{
-//							na += "</span>";
-							naa.Add(taa);
-							taa = new ConsoleItem(); //if something without span, null will be filtered out
-						}
-						if (pa != def_attr)
-						{
-							if (pa == -1)
-							{
-//								na += "<span class=\"termReverse\">";
-								naa.Add(taa); //if something without span, null will be filtered out
-								taa = new ConsoleItem
-								{
-									fc = ConsoleColor.Black,
-									bc = ConsoleColor.Green
-								};
-							}
-							else
-							{
-//								na += "<span style=\"";
-								naa.Add(taa); //if something without span, null will be filtered out
-								taa = new ConsoleItem();
-								var ra = (pa >> 3) & 7;
-								var sa = pa & 7;
-								if (ra != 7)
-								{
-//									na += "color:" + fg_colors[ra] + ';';
-									taa.fc = fgColors[ra];
-								}
-								if (sa != 0)
-								{
-//									na += "background-color:" + bg_colors[sa] + ';';
-									taa.bc = bgColors[sa];
-								}
-//								na += "\">";
-							}
-						}
-					}
-					switch (c)
-					{
-						case 32:
-//							na += "&nbsp;";
-							taa.c += " ";
-							break;
-						case 38:
-//							na += "&amp;";
-							taa.c += "&";
-							break;
-						case 60:
-//							na += "&lt;";
-							taa.c += "<";
-							break;
-						case 62:
-//							na += "&gt;";
-							taa.c += ">";
-							break;
-						default:
-							if (c < 32)
-							{
-								//na += "&nbsp;";
-								taa.c += " ";
-							}
-							else
-							{
-								//na += (char) c;
-								taa.c += (char)c;
-							}
-							break;
-					}
-					qa = pa;
-				}
+			var naa = new List<ConsoleItem>();
+			var w = width;
+			var oa = -1;
+			var qa = def_attr;
+			int i;
+			var taa = new ConsoleItem();
+			for (i = 0; i < w; i++)
+			{
+				qa = RefreshOne(ia, i, oa, qa, naa, ref taa);
+			}
+			if (qa != def_attr)
+			{
+				//na += "</span>";
+				naa.Add(taa);
+			}
+			naa.Add(taa);
+			Console.SetCursorPosition(0, row);
+			foreach (var aa in naa)
+			{
+				Render(aa);
+			}
+		}
+
+		private static void Render(ConsoleItem aa)
+		{
+			if (aa.c == null)
+				return;
+			Console.ForegroundColor = aa.fc;
+			Console.BackgroundColor = aa.bc;
+			Console.Write(aa.c);
+		}
+
+		private int RefreshOne(int[] ia, int i, int oa, int qa, List<ConsoleItem> naa, ref ConsoleItem taa)
+		{
+			var c = ia[i];
+			var pa = (int) c >> 16;
+			c &= 0xffff;
+			if (i == oa)
+			{
+				pa = -1;
+			}
+			if (pa != qa)
+			{
 				if (qa != def_attr)
 				{
-					//na += "</span>";
+//							na += "</span>";
 					naa.Add(taa);
+					taa = new ConsoleItem(); //if something without span, null will be filtered out
 				}
-				naa.Add(taa);
-				Console.SetCursorPosition(0, row);
-				foreach (var aa in naa)
+				if (pa != def_attr)
 				{
-					if(aa.c == null)
-						continue;
-					Console.ForegroundColor = aa.fc;
-					Console.BackgroundColor = aa.bc;
-					Console.Write(aa.c);
+					if (pa == -1)
+					{
+//								na += "<span class=\"termReverse\">";
+						naa.Add(taa); //if something without span, null will be filtered out
+						taa = new ConsoleItem
+						{
+							fc = ConsoleColor.Black,
+							bc = ConsoleColor.Green
+						};
+					}
+					else
+					{
+//								na += "<span style=\"";
+						naa.Add(taa); //if something without span, null will be filtered out
+						taa = new ConsoleItem();
+						var ra = (pa >> 3) & 7;
+						var sa = pa & 7;
+						if (ra != 7)
+						{
+//									na += "color:" + fg_colors[ra] + ';';
+							taa.fc = fgColors[ra];
+						}
+						if (sa != 0)
+						{
+//									na += "background-color:" + bg_colors[sa] + ';';
+							taa.bc = bgColors[sa];
+						}
+//								na += "\">";
+					}
 				}
 			}
-		}
-
-		private class ConsoleItem
-		{
-			public ConsoleColor fc = ConsoleColor.Gray;
-			public ConsoleColor bc = ConsoleColor.Black;
-			public string c;
-		}
-
-		private void ProcessEscapeCharacter(char ch)
-		{
-			if (escapeChars.Length == 0 && "78".IndexOf(ch) >= 0)
+			switch (c)
 			{
-				escapeChars += ch.ToString(CultureInfo.InvariantCulture);
-			}
-			else if (escapeChars.Length > 0 && "()Y".IndexOf(escapeChars[0]) >= 0)
-			{
-				escapeChars += ch.ToString(CultureInfo.InvariantCulture);
-				if (escapeChars.Length != (escapeChars[0] == 'Y' ? 3 : 2)) return;
-			}
-			else if (ch == ';' || char.IsDigit(ch))
-			{
-				escapeArgs += ch.ToString(CultureInfo.InvariantCulture);
-				return;
-			}
-			else
-			{
-				escapeChars += ch.ToString(CultureInfo.InvariantCulture);
-				if ("[#?()Y".IndexOf(ch) >= 0) return;
-			}
-			ProcessEscapeSequence();
-			escapeChars = null;
-			escapeArgs = null;
-		}
-
-		private void ProcessEscapeSequence()
-		{
-			if (escapeChars.StartsWith("Y"))
-			{
-				row = escapeChars[1] - 64;
-				column = escapeChars[2] - 64;
-				return;
-			}
-			if (vt52Mode && (escapeChars == "D" || escapeChars == "H")) escapeChars += "_";
-
-			var args = escapeArgs.Split(';');
-			var arg0 = args.Length > 0 && args[0] != "" ? int.Parse(args[0]) : (int?) null;
-			switch (escapeChars)
-			{
-				case "[A":
-				case "A":
-					row -= Math.Max(arg0 ?? 1, 1);
+				case 32:
+//							na += "&nbsp;";
+					taa.c += " ";
 					break;
-				case "[B":
-				case "B":
-					row += Math.Max(arg0 ?? 1, 1);
+				case 38:
+//							na += "&amp;";
+					taa.c += "&";
 					break;
-				case "[c":
-				case "C":
-					column += Math.Max(arg0 ?? 1, 1);
+				case 60:
+//							na += "&lt;";
+					taa.c += "<";
 					break;
-				case "[D":
-				case "D":
-					column -= Math.Max(arg0 ?? 1, 1);
+				case 62:
+//							na += "&gt;";
+					taa.c += ">";
 					break;
-
-				case "[f":
-				case "[H":
-				case "H_":
-					row = Math.Max(arg0 ?? 1, 1) - 1;
-					column = Math.Max(arg0 ?? 1, 1) - 1;
-					break;
-
-				case "M":
-					PriorRowWithScroll();
-					break;
-				case "D_":
-					NextRowWithScroll();
-					break;
-				case "E":
-					NextRowWithScroll();
-					column = 0;
-					break;
-
-				case "[r":
-					scrollTop = (arg0 ?? 1) - 1;
-					scrollBottom = (arg0 ?? height);
-					break;
-
-				case "H":
-					if (!tabStops.Contains(column)) tabStops.Add(column);
-					break;
-				case "g":
-					if (arg0 == 3) tabStops.Clear();
-					else tabStops.Remove(column);
-					break;
-
-				case "[J":
-				case "J":
-					switch (arg0 ?? 0)
+				default:
+					if (c < 32)
 					{
-						case 0:
-							ClearRange(row, column, height, width);
-							break;
-						case 1:
-							ClearRange(0, 0, row, column + 1);
-							break;
-						case 2:
-							ClearRange(0, 0, height, width);
-							break;
+						//na += "&nbsp;";
+						taa.c += " ";
+					}
+					else
+					{
+						//na += (char) c;
+						taa.c += (char) c;
 					}
 					break;
-				case "[K":
-				case "K":
-					switch (arg0 ?? 0)
-					{
-						case 0:
-							ClearRange(row, column, row, width);
-							break;
-						case 1:
-							ClearRange(row, 0, row, column + 1);
-							break;
-						case 2:
-							ClearRange(row, 0, row, width);
-							break;
-					}
-					break;
-
-				case "?l":
-				case "?h":
-					var h = escapeChars == "?h";
-					switch (arg0)
-					{
-						case 2:
-							vt52Mode = h;
-							break;
-						case 3:
-							width = h ? 132 : 80;
-							ResetBufferAndWindows();
-							break;
-						case 7:
-							autoWrapMode = h;
-							break;
-					}
-					break;
-				case "<":
-					vt52Mode = false;
-					break;
-
-				case "#3":
-				case "#4":
-				case "#5":
-				case "#6":
-					break;
-
-				case "[s":
-					saveRow = row;
-					saveColumn = column;
-					break;
-				case "7":
-					saveRow = row;
-					saveColumn = column;
-					break;
-				case "[u":
-					row = saveRow;
-					column = saveColumn;
-					break;
-				case "8":
-					row = saveRow;
-					column = saveColumn;
-					break;
-
-				case "c":
-					Reset();
-					break;
 			}
-			if (column < 0) column = 0;
-			if (column >= width) column = width - 1;
-			if (row < 0) row = 0;
-			if (row >= height) row = height - 1;
-		}
-
-		private void PriorRowWithScroll()
-		{
-			if (row == scrollTop) ScrollDown();
-			else row--;
-		}
-
-		private void NextRowWithScroll()
-		{
-			if (row == scrollBottom - 1) ScrollUp();
-			else row++;
-		}
-
-		private void ScrollUp()
-		{
-			Array.Copy(buffer, width*(scrollTop + 1), buffer, width*scrollTop, width*(scrollBottom - scrollTop - 1));
-			for (var r = 0; r < height-1; r++)
-			{
-				var line = new string(buffer.Skip(r * width).Take(width).Select(x => x == 0 ? ' ' : x).ToArray());
-				Console.SetCursorPosition(0, r);
-				Console.Write(line);
-			}
-
-			ClearRange(scrollBottom - 1, 0, scrollBottom - 1, width);
-			UpdateLines();
-		}
-
-		private void ScrollDown()
-		{
-			Array.Copy(buffer, width*scrollTop, buffer, width*(scrollTop + 1), width*(scrollBottom - scrollTop - 1));
-			for (var r = 0; r < height - 1; r++)
-			{
-				var line = new string(buffer.Skip(r * width).Take(width).Select(x => x == 0 ? ' ' : x).ToArray());
-				Console.SetCursorPosition(0, r);
-				Console.Write(line);
-			}
-			ClearRange(scrollTop, 0, scrollTop, width);
-			UpdateLines();
-		}
-
-		private void ClearRange(int startRow, int startColumn, int endRow, int endColumn)
-		{
-			var start = startRow*width + startColumn;
-			var end = endRow*width + endColumn;
-			for (var i = start; i < end; i++)
-			{
-				const char empty = ' ';
-				buffer[i] = empty;
-
-				var col = i%width;
-				var row = i/width;
-				BufferAt(row, col, empty);
-			}
+			qa = pa;
+			return qa;
 		}
 
 		private void UpdateLines()
@@ -864,13 +543,13 @@ namespace PCEmulator.Net
 					break;
 				case ConsoleKey.UpArrow:
 					if ((@event.Modifiers & ConsoleModifiers.Control) == ConsoleModifiers.Control)
-						ScrollDown();
+						this.scroll_disp(-1);
 					else
 						@char = "\x1b[A";
 					break;
 				case ConsoleKey.DownArrow:
 					if ((@event.Modifiers & ConsoleModifiers.Control) == ConsoleModifiers.Control)
-						ScrollUp();
+						this.scroll_disp(1);
 					else
 						@char = "\x1b[B";
 					break;
@@ -888,13 +567,13 @@ namespace PCEmulator.Net
 					break;
 				case ConsoleKey.PageUp:
 					if ((@event.Modifiers & ConsoleModifiers.Control) == ConsoleModifiers.Control)
-						ScrollUp(); //TODO: this.scroll_disp(-(this.h - 1));
+						this.scroll_disp(-(this.height - 1));
 					else
 						@char = "\x1b[5~";
 					break;
 				case ConsoleKey.PageDown:
 					if ((@event.Modifiers & ConsoleModifiers.Control) == ConsoleModifiers.Control)
-						ScrollDown(); //TODO: this.scroll_disp(this.h - 1);
+						this.scroll_disp(this.height - 1);
 					else
 						@char = "\x1b[6~";
 					break;
@@ -927,6 +606,42 @@ namespace PCEmulator.Net
 				//this.show_cursor(); //TODO; implement
 				Array.ForEach(@char.ToArray(), c => termHandler(c));
 			}
+		}
+
+		private void scroll_disp(int n)
+		{
+			if (n >= 0)
+			{
+				for (var i = 0; i < n; i++)
+				{
+					if (this.y_disp == this.y_base)
+						break;
+					if (++this.y_disp == this.cur_h)
+						this.y_disp = 0;
+				}
+			}
+			else
+			{
+				n = -n;
+				var ta = this.y_base + this.height;
+				if (ta >= this.cur_h)
+					ta -= this.cur_h;
+				for (var i = 0; i < n; i++)
+				{
+					if (this.y_disp == ta)
+						break;
+					if (--this.y_disp < 0)
+						this.y_disp = this.cur_h - 1;
+				}
+			}
+			this.Refresh(0, this.height - 1);
+		}
+
+		private class ConsoleItem
+		{
+			public ConsoleColor fc = ConsoleColor.Gray;
+			public ConsoleColor bc = ConsoleColor.Black;
+			public string c;
 		}
 	}
 }
