500cd216135997ada92d629d39c7b834b7bde8b6
xcrash <grishinage@gmail.com>
2014-07-05 19:58:04 +0400

    download dependencies

---
+++ src/PCEmulator.Net/DownloadBinaries.cmd
@@ -0,0 +1,3 @@
+If Not Exist vmlinux-2.6.20.bin wget --no-check-certificate https://github.com/levskaya/jslinux-deobfuscated/raw/master/vmlinux-2.6.20.bin
+If Not Exist root.bin wget --no-check-certificate https://github.com/levskaya/jslinux-deobfuscated/raw/master/root.bin
+If Not Exist linuxstart.bin wget --no-check-certificate https://github.com/levskaya/jslinux-deobfuscated/raw/master/linuxstart.bin
