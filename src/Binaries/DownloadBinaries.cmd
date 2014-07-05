89c66043c2caaae2002873e9093c3b55bff41358
xcrash <grishinage@gmail.com>
2014-07-05 20:25:00 +0400

    reference to assembly to download binaries

---
+++ src/Binaries/DownloadBinaries.cmd
@@ -0,0 +1,3 @@
+If Not Exist vmlinux-2.6.20.bin ..\packages\WGETWindows.1.11.4\wget.exe --no-check-certificate https://github.com/levskaya/jslinux-deobfuscated/raw/master/vmlinux-2.6.20.bin
+If Not Exist root.bin ..\packages\WGETWindows.1.11.4\wget.exe --no-check-certificate https://github.com/levskaya/jslinux-deobfuscated/raw/master/root.bin
+If Not Exist linuxstart.bin ..\packages\WGETWindows.1.11.4\wget.exe --no-check-certificate https://github.com/levskaya/jslinux-deobfuscated/raw/master/linuxstart.bin
--- src/PCEmulator.Net.Tests/DownloadBinaries.cmd
+++
@@ -1,3 +0,0 @@
-If Not Exist vmlinux-2.6.20.bin wget --no-check-certificate https://github.com/levskaya/jslinux-deobfuscated/raw/master/vmlinux-2.6.20.bin
-If Not Exist root.bin wget --no-check-certificate https://github.com/levskaya/jslinux-deobfuscated/raw/master/root.bin
-If Not Exist linuxstart.bin wget --no-check-certificate https://github.com/levskaya/jslinux-deobfuscated/raw/master/linuxstart.bin
