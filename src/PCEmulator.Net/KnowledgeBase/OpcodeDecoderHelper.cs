6f88cff3a29a2d0142239aabb8686a0ddf1e5bae
xcrash <grishinage@gmail.com>
2014-07-07 03:26:34 +0400

    add helper
    Conflicts:
    	src/PCEmulator.Net/PCEmulator.Net.csproj

---
+++ src/PCEmulator.Net/KnowledgeBase/OpcodeDecoderHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace PCEmulator.Net.KnowledgeBase
+{
+	public enum OperationDirection
+	{
+		RegistryToMemory = 0,
+		MemoryToRegistry = 1
+	}
+
+	public enum OperationOperandSize
+	{
+		EightBit = 0,
+		SixteenBitOr32Bit = 1
+	}
+
+	public static class OpcodeDecoderHelper
+	{
+		public static OperationDirection GetDirection(byte opbyte)
+		{
+			return ((opbyte >> 1) & 1) == 0
+				? OperationDirection.RegistryToMemory
+				: OperationDirection.MemoryToRegistry;
+		}
+
+		public static OperationOperandSize GetOperandSize(byte opbyte)
+		{
+			return (opbyte & 1) == 0
+				? OperationOperandSize.EightBit
+				: OperationOperandSize.SixteenBitOr32Bit;
+		}
+	}
+}
